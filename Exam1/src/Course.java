83737dead28b331acce8704bedfd4a1dd1ed21e1
XGYRXM <534905834@qq.com>
2016-09-01 17:29:49 +0800

    21
    saa

---
+++ Exam1/src/Course.java
@@ -0,0 +1,5 @@
+
+public class Course {
+	Class Dog{ËÑË÷      private String name;      private String color;      private int age;      Dog(String name, String color, int age)      {                  this.name = name;                 this.color = color;                 this.age = age;      }         public void displayInfo()       {             System.out.println("name=" + name);             System.out.println("color=" + color);             System.out.println("age=" + age);      }}public class TestDog{       public static void main(String[] args)       {               Dog dog = new dog("Ð¡°×", "°×É«", "3");               dog.displayInfo();       }}
+
+}
---
+++ Exam1/src/test.java
@@ -0,0 +1,4 @@
+Public class point
+
+Class Dog{ËÑË÷      private String name;      private String color;      private int age;      Dog(String name, String color, int age)      {                  this.name = name;                 this.color = color;                 this.age = age;      }         public void displayInfo()       {             S
+	
